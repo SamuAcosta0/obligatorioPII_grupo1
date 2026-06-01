c30d941031a08dc53e6078003992e8c6fe917dce
lucasbeka <lbekavac@correo.um.edu.uy>
2026-06-01 08:13:03 -0300

    con parser

--- src/main/java/uy/edu/um/Main.java
+++ src/main/java/uy/edu/um/Main.java
@@ -3,8 +3,10 @@ package uy.edu.um;
 import uy.edu.um.doors.ProcessConsole;
 import uy.edu.um.doors.ProcessManagerImpl;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         ProcessConsole pc = new ProcessConsole(new ProcessManagerImpl());
         pc.init();
