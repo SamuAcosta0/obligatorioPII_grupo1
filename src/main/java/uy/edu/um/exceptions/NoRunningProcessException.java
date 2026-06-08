64829b54e159594b2c251c2a63caf878dfc70dcf
SamuAcosta0 <vilarangel3@gmail.com>
2026-06-08 16:08:13 -0300

    ULTIMOS-MENSAJES

--- src/main/java/uy/edu/um/exceptions/NoRunningProcessException.java
+++
@@ -1,7 +0,0 @@
-package uy.edu.um.exceptions;
-
-public class NoRunningProcessException extends RuntimeException {
-    public NoRunningProcessException(String message) {
-        super(message);
-    }
-}
